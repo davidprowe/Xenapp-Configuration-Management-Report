fcb87535878678ae2fbc1b2d0d604263b96e1230
davidprowe <davidprowe@gmail.com>
2015-02-05 02:39:52 -0500

    Added option to remove 'Server NAME was installed'
    these entries are commonly added if you xenapp prep with PVS images
    before shutting them down

--- XenappConfigManagementCheck.ps1
+++ XenappConfigManagementCheck.ps1
@@ -4,8 +4,18 @@ $ctxoutfile = "C:\tools\Powershell\xenappconfigmgmt.html"
 $days = -7
 $dayspositive = [math]::abs($days)
 $citrixudl = "C:\tools\Powershell\xenappconfigmgmt.udl"
+#do you want to be notified if a server is added - commonly happens if you xenappprep with pvs, change to yes if you do
+$added = 'no'
 Add-PSSnapin Citrix.Common.Commands
-Get-CtxConfigurationLogReport -datalinkpath $citrixudl -TimePeriodFrom (get-date ((get-date).AddDays($days)) -UFormat %m/%d/%y) -TimePeriodTo (get-date -UFormat %m/%d/%y)|ConvertTo-Html|out-file $ctxoutfile
+if ($added -eq 'yes')
+{
+Get-CtxConfigurationLogReport -datalinkpath $citrixudl -TimePeriodFrom (get-date ((get-date).AddDays($days)) -UFormat %m/%d/%y) -TimePeriodTo (get-date -UFormat %m/%d/%y)|Where-Object -Property Description -notlike "server*was installed"|ConvertTo-Html|out-file $ctxoutfile
+}
+else{
+$h = Get-CtxConfigurationLogReport -datalinkpath $citrixudl -TimePeriodFrom (get-date ((get-date).AddDays($days)) -UFormat %m/%d/%y) -TimePeriodTo (get-date -UFormat %m/%d/%y)|ConvertTo-csv -NoTypeInformation|Convertfrom-csv
+#remove server was installed due to PVS always reporting server additions after reboot
+$H |Where-Object -Property description -notlike "Server*was installed*"|ConvertTo-Html|Out-File $ctxoutfile
+}
 
 $emailFrom     = "servername@domain.org"
 $emailTo       = "group@domain.org"
